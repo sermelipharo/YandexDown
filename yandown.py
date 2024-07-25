8d6ce6c18cd51a822a8084d45e14d6cfc17f3e55
sermelipharo <sermelipharo@users.noreply.github.com>
2024-07-25 17:34:54 +0400

    Update yandown.py

--- yandown.py
+++ yandown.py
@@ -2,18 +2,22 @@ import argparse
 import requests
 import urllib.parse
 import os
+import sys
 
 
 class YandexDiskDownloader:
-    def __init__(self, link, download_location):
+    def __init__(self, link, download_location, custom_name=None):
         self.link = link
         self.download_location = os.path.expanduser(download_location)
+        self.custom_name = custom_name
 
     def download(self):
         url = f"https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={self.link}"
         response = requests.get(url)
         download_url = response.json()["href"]
-        file_name = urllib.parse.unquote(download_url.split("filename=")[1].split("&")[0])
+        original_file_name = urllib.parse.unquote(download_url.split("filename=")[1].split("&")[0])
+        file_extension = os.path.splitext(original_file_name)[1]
+        file_name = self.custom_name + file_extension if self.custom_name else original_file_name
         save_path = os.path.join(self.download_location, file_name)
 
         # Ensure the download directory exists
@@ -31,27 +35,46 @@ class YandexDiskDownloader:
 
 def download_from_file(file_path, download_location):
     with open(file_path, 'r') as file:
-        links = file.readlines()
+        lines = file.readlines()
         
-    for link in links:
-        link = link.strip()
-        if link:
-            downloader = YandexDiskDownloader(link, download_location)
+    for line in lines:
+        line = line.strip()
+        if line:
+            if ' ' in line:
+                link, custom_name = line.split(maxsplit=1)
+            elif ',' in line:
+                link, custom_name = line.split(',', maxsplit=1)
+            elif ';' in line:
+                link, custom_name = line.split(';', maxsplit=1)
+            else:
+                link, custom_name = line, None
+
+            custom_name = custom_name.strip() if custom_name else None
+            downloader = YandexDiskDownloader(link, download_location, custom_name)
             downloader.download()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Yandex Disk Downloader')
     parser.add_argument('-l', '--link', type=str, help='Link for Yandex Disk URL')
-    parser.add_argument('-d', '--download_location', type=str, help='Download location in PC', required=True)
+    parser.add_argument('-d', '--download_location', type=str, help='Download location in PC')
     parser.add_argument('-f', '--file', type=str, help='Path to file with Yandex Disk URLs')
 
     args = parser.parse_args()
 
     if args.file:
-        download_from_file(args.file, args.download_location)
+        if not args.download_location:
+            print("Error: You must provide a download location when using a file.")
+        else:
+            download_from_file(args.file, args.download_location)
     elif args.link:
-        downloader = YandexDiskDownloader(args.link, args.download_location)
+        download_location = args.download_location if args.download_location else os.getcwd()
+        downloader = YandexDiskDownloader(args.link, download_location)
+        downloader.download()
+    elif len(sys.argv) == 2:
+        download_location = os.getcwd()
+        link = sys.argv[1]
+        downloader = YandexDiskDownloader(link, download_location)
         downloader.download()
     else:
         print("Error: You must provide either a link or a file containing links.")
